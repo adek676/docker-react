b32f27bc8f6c2998afb7ccc6e8402ae2779cfa83
Adrian Holody <adek676@gmail.com>
2023-05-16 20:28:05 +0200

    change App.js text

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          hello there2
+          hello there on feature branch
         </p>
         <a
           className="App-link"
